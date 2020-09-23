601b099beb218a57f00ff5d2bce9884d329d3ff8
royleochan <royleochan@gmail.com>
2020-09-24 00:12:32 +0800

    Create NavBar component

---
+++ components/NavBar/NavBar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./NavBar.module.scss";
+
+const NavBar = (props) => {
+  return (
+    <div className={styles.header}>
+      <p>Header</p>
+    </div>
+  );
+};
+
+export default NavBar;
