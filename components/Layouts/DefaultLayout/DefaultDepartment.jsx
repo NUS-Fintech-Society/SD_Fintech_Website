0d420a612b5712bb450d2dc9ed3122a315c34d08
Cheng Wun Jing <chengwunjing@outlook.com>
2020-10-28 14:42:53 +0800

    merged

--- components/Layouts/DefaultLayout/DefaultDepartment.jsx
+++ components/Layouts/DefaultLayout/DefaultDepartment.jsx
@@ -7,46 +7,49 @@ import Footer from "components/Footer/Footer";
 
 const DefaultDepartment = (props) => {
   return (
-  <div className={styles.layout}>
-    <Head>
-      <title>NUS FinTech Society</title>
-      <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
-    </Head>
-    <NavBar />
-    <div className={styles.title}>
-    {props.children[0]}
-    </div>
-    <div className={styles.flex}>
-      <div className={styles.blank}></div>
-      <div className={styles.container}>
-        <div className={styles.boxtitle}>
-          <h3>Purpose</h3>
+    <div>
+      <Head>
+        <title>NUS FinTech Society</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
+      </Head>
+      <NavBar />
+      <div className={styles.layout}>
+        
+        <div className={styles.title}>
+          {props.children[0]}
         </div>
-        <div className={styles.boxbody}>
-          {props.children[1]}
-        </div>
-        <hr className={styles.line}></hr>
-        <div className={styles.imageleft}>
-      
-        </div>
-      </div>
- 
-      <div className={styles.container}>
-        <div className={styles.boxtitleright}>
-          <h3>Goal</h3>
-        </div>
-        <div className={styles.boxbodyright}>
-          {props.children[2]}
-        </div>
-        <hr className={styles.lineright}></hr>
-        <div className={styles.image}>
-        </div>
-      </div>
-      <div className={styles.blank}></div>
-      </div>
+        <div className={styles.flex}>
 
-    <Footer />
-  </div>
+          <div className={styles.blank}></div>
+          <div className={styles.container}>
+            <div className={styles.boxtitle}>
+              <h3>Purpose</h3>
+            </div>
+            <div className={styles.boxbody}>
+              {props.children[1]}
+            </div>
+            <hr className={styles.line}></hr>
+            <div className={styles.imageleft}>
+
+            </div>
+          </div>
+
+          <div className={styles.container}>
+            <div className={styles.boxtitleright}>
+              <h3>Goal</h3>
+            </div>
+            <div className={styles.boxbodyright}>
+              {props.children[2]}
+            </div>
+            <hr className={styles.lineright}></hr>
+            <div className={styles.image}>
+            </div>
+          </div>
+          <div className={styles.blank}></div>
+        </div>
+      </div>
+      <Footer />
+    </div>
 
   );
 };
