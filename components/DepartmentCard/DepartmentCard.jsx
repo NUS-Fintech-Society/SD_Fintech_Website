4373e488820541f2a8a8b2d97c79557984c5c741
royleochan <royleochan@gmail.com>
2020-11-01 02:51:24 +0800

    Add department card component

---
+++ components/DepartmentCard/DepartmentCard.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+import Link from "next/link";
+import styles from "./DepartmentCard.module.scss";
+import Button from "@material-ui/core/Button";
+
+const DepartmentCard = (props) => {
+  const { icon, title, description, route } = props;
+  console.log(route);
+  return (
+    <div className={styles.container}>
+      <img src={icon} alt={title} className={styles.image} />
+      <h3>{title}</h3>
+      <p>{description}</p>
+      <Link href={route} passHref={true}>
+        <Button component="a" variant="contained" className={styles.button}>
+          View
+        </Button>
+      </Link>
+    </div>
+  );
+};
+
+export default DepartmentCard;
