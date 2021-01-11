579b3fd12907a7e23ac7bb1244f26b982509e5e7
marcusoyh <ongmarcus204@gmail.com>
2021-01-11 15:55:27 +0800

    margin top added

--- components/Layouts/DefaultDepartmentLayout/DefaultDepartmentLayout.jsx
+++ components/Layouts/DefaultDepartmentLayout/DefaultDepartmentLayout.jsx
@@ -27,12 +27,16 @@ const DefaultDepartmentLayout = (props) => {
   return (
     <div>
       <DefaultLayout>
+        <div
+        style={{ marginTop: "2rem" }}
+        >
       {data && <DeptInfo deptInfo={data} />}
         <div style={{backgroundColor:'rgba(196, 196, 196, 0.2)',paddingBottom:'3%'}}>
           {data &&
             data.projects.map((project, index) => (
               <ProjLayout key={project.id} project={project} index={index} deptId={deptId}/>
             ))}
+        </div>
         </div>
       </DefaultLayout>
     </div>
