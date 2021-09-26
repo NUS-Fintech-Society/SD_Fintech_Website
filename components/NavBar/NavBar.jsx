d100eb316e9cbbb867ecd8d8b3fc43e7a6fc6ddc
Cassia <e0564059@u.nus.edu>
2021-09-26 23:53:54 +0800

    New Notice component with scss styling

--- components/NavBar/NavBar.jsx
+++ components/NavBar/NavBar.jsx
@@ -1,81 +1,57 @@
 import React, { useState } from "react";
 import styles from "./NavBar.module.scss";
 import Link from "next/link";
-import Alert from "@material-ui/lab/Alert";
+
 import DrawerToggleButton from "components/SideDrawer/DrawerToggleButton";
-import InfoIcon from "@material-ui/icons/Info";
-import { createMuiTheme, ThemeProvider } from "@material-ui/core";
-import { makeStyles } from "@material-ui/core";
 
-const useStyles = makeStyles({
-  myAlert: {
-    backgroundColor: "white",
-  },
-});
-
-//Colour of banner is not updated
-const theme = createMuiTheme({
-  typography: {
-    fontFamily: "Lato",
-  },
-});
+import Notice from "components/Notice/Notice";
 
 const NavBar = (props) => {
-  const classes = useStyles();
   return (
-    <ThemeProvider theme={theme}>
-      <header className={styles.navbar}>
-        <nav className={styles.navbar_navigation}>
-          <div className={styles.navbar_toggle_button}>
-            <DrawerToggleButton click={props.drawerClickHandler} />
-          </div>
-          <div className={styles.navbar_logo}>
-            <img
-              src="/images/SocietyLogo.png"
-              alt="society logo"
-              className={styles.image}
-            />
-          </div>
-          <div className={styles.spacer}></div>
-          <div className={styles.navbar_items}>
-            <ul id="nav-mobile" className="center">
-              <li>
-                <Link href="/">Home</Link>
-              </li>
-              <li>
-                <Link href="/events">Events</Link>
-              </li>
-
-              <div className={styles.dropdown}>
-                <Link className={styles.navbar_items} href="/#ourTeam">
-                  Our Team
-                </Link>
-                <div className={styles.dropdown_content}>
-                  <a href="/department/operations">Operations</a>
-                  <a href="/department/machinelearning">Machine Learning</a>
-                  <a href="/department/blockchain">Blockchain</a>
-                  <a href="/department/devops">DevOps</a>
-                </div>
-              </div>
-
-              <li>
-                <Link href="/#contact">Contact Us</Link>
-              </li>
-            </ul>
-          </div>
-        </nav>
-        <div>
-          <Alert
-            icon={<InfoIcon fontSize="inherit" />}
-            severity="info"
-            className={classes.myAlert}
-          >
-            <strong>Notice</strong> — This is for notifications! Testing
-            sentences are as follows: One Two Three Four Five
-          </Alert>
+    <header className={styles.navbar}>
+      <nav className={styles.navbar_navigation}>
+        <div className={styles.navbar_toggle_button}>
+          <DrawerToggleButton click={props.drawerClickHandler} />
         </div>
-      </header>
-    </ThemeProvider>
+        <div className={styles.navbar_logo}>
+          <img
+            src="/images/SocietyLogo.png"
+            alt="society logo"
+            className={styles.image}
+          />
+        </div>
+        <div className={styles.spacer}></div>
+        <div className={styles.navbar_items}>
+          <ul id="nav-mobile" className="center">
+            <li>
+              <Link href="/">Home</Link>
+            </li>
+            <li>
+              <Link href="/events">Events</Link>
+            </li>
+
+            <div className={styles.dropdown}>
+              <Link className={styles.navbar_items} href="/#ourTeam">
+                Our Team
+              </Link>
+              <div className={styles.dropdown_content}>
+                <a href="/department/operations">Operations</a>
+                <a href="/department/machinelearning">Machine Learning</a>
+                <a href="/department/blockchain">Blockchain</a>
+                <a href="/department/devops">DevOps</a>
+              </div>
+            </div>
+
+            <li>
+              <Link href="/#contact">Contact Us</Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      <div>
+        <Notice></Notice>
+      </div>
+    </header>
   );
 };
 
