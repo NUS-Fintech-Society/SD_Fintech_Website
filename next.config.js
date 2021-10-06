e098e8f79fb065462a5bc6786f6041dd88b93762
jerrenss <gerrenseow@gmail.com>
2021-10-07 00:18:48 +0800

    next config changes

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 const ghPages = process.env.DEPLOY_TARGET === 'gh-pages';
 
 module.exports = {
-  assetPrefix: ghPages ? '/nus-fintech-society.github.io/SD_Fintech_Website/' : ''
+  assetPrefix: ghPages ? '/SD_Fintech_Website/' : ''
 };
