15a58f137b79e09dd8274b3921fc6af4bda52122
jerrenss <gerrenseow@gmail.com>
2021-10-07 00:12:26 +0800

    added log to force rebuild

--- pages/index.js
+++ pages/index.js
@@ -28,6 +28,8 @@ const Home = (props) => {
   const { register, handleSubmit, errors } = useForm();
   const [formState, setFormState] = useState({});
 
+  console.log("TEST: TO BE REMOVED")
+
   useEffect(() => {
     loadDepartments();
     fetchEvents().then(() => {
