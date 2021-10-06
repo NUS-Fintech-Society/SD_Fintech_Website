12d7a3060562ec942f531ae4cacdb25f911e2151
jerrenss <gerrenseow@gmail.com>
2021-10-07 00:45:16 +0800

    next config changes

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 const ghPages = process.env.DEPLOY_TARGET === 'gh-pages';
 
 module.exports = {
+  basePath: ghPages ? '/SD_Fintech_Website/' : '',
   assetPrefix: ghPages ? '/SD_Fintech_Website/' : ''
 };
