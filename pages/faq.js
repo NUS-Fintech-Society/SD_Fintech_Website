761db374632235cefd0a7e4a82cdbc9c28b516c2
jerrenss <gerrenseow@gmail.com>
2021-11-01 01:51:54 +0800

    add faq page

---
+++ pages/faq.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import DefaultLayout from "components/Layouts/DefaultLayout/DefaultLayout";
+
+const Faq = (props) => {
+return(
+    <div>
+        <DefaultLayout>
+            <h1 style={{fontSize:60, marginTop: 300, textAlign: 'center'}}>
+                Work in progress
+            </h1>
+        </DefaultLayout>
+    </div>
+    );
+};
+
+export default Faq; 
