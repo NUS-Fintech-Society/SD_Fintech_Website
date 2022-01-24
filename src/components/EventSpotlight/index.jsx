bb716258b4c05f70749e5aea8eb1a091b1a09297
Ivander <ivanderjmw@gmail.com>
2022-01-24 23:30:49 +0800

    create event spotlight component

---
+++ src/components/EventSpotlight/index.jsx
@@ -0,0 +1,122 @@
+import { Box, Typography } from '@material-ui/core'
+import { LocationOn, Event as EventIcon } from '@material-ui/icons'
+import { makeStyles } from '@material-ui/styles'
+import moment from 'moment'
+import ClampLines from 'react-clamp-lines'
+import PropTypes from 'prop-types'
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    boxShadow: '0px 10px 13px -7px #efefef, 5px 5px 15px 5px rgba(0,0,0,0)',
+    maxWidth: '40em',
+    padding: '1em',
+    transform: 'translateY(0px);',
+    transition: '0.5s',
+    marginTop: '20px',
+    borderRadius: '1.5em',
+
+    [theme.breakpoints.down('xs')]: {
+      width: '100%',
+    },
+    '&:hover': {
+      boxShadow: '0px 10px 13px -7px #000000, 5px 5px 15px 5px rgba(0,0,0,0)',
+
+      transform: 'translateY(-10px);',
+      backgroundColor: '#f5f5f5',
+    },
+  },
+  cardHeader: {
+    display: 'inline-block',
+  },
+  date: {
+    display: 'flex',
+    gap: '0.5em',
+    '& .label': {
+      display: 'flex',
+      alignItems: 'center',
+      color: '#4B87B1',
+    },
+  },
+  location: {
+    display: 'flex',
+    gap: '0.5em',
+    '& .label': {
+      display: 'flex',
+      alignItems: 'center',
+      color: '#4B87B1',
+    },
+  },
+  imageCrop: {
+    borderRadius: '1em',
+    overflow: 'hidden',
+    maxHeight: '400px',
+    '& img': {
+      maxWidth: '100%',
+      height: 'auto !important',
+      display: 'block',
+      width: 'auto\\9',
+    },
+  },
+  description: {
+    maxHeight: '10em',
+    overflow: 'hidden',
+    textOverflow: 'ellipsis',
+  },
+}))
+
+const EventSpotlight = ({ date, location, title, description, imageUrl }) => {
+  const classes = useStyles()
+
+  return (
+    <>
+      <Box className={classes.root}>
+        <Box className={classes.imageCrop}>
+          <img
+            src={imageUrl || 'default-event.jpg'}
+            className={classes.image}
+          />
+        </Box>
+        <Box className={classes.cardHeader}>
+          <Box>
+            <Typography variant="h4">{title}</Typography>
+          </Box>
+          <Box className={classes.date}>
+            <span className="label">
+              <EventIcon />
+              Date:
+            </span>{' '}
+            {moment(date).format('MMMM DD YYYY')}
+          </Box>
+          <Box>
+            <Box className={classes.location}>
+              <span className="label">
+                <LocationOn />
+                Location:
+              </span>{' '}
+              {location}
+            </Box>
+          </Box>
+        </Box>
+        <Box className={classes.description}>
+          <Typography variant="body">
+            <ClampLines
+              lines={5}
+              text={description}
+              buttons={false}
+            ></ClampLines>
+          </Typography>
+        </Box>
+      </Box>
+    </>
+  )
+}
+
+EventSpotlight.propTypes = {
+  date: PropTypes.object,
+  location: PropTypes.string,
+  title: PropTypes.string,
+  description: PropTypes.string,
+  imageUrl: PropTypes.string,
+}
+
+export default EventSpotlight
--- src/views/Events/index.jsx
+++ src/views/Events/index.jsx
@@ -1,6 +1,7 @@
 import { Box, Container, makeStyles } from '@material-ui/core'
 import moment from 'moment'
 import EventCard from '../../components/EventCard'
+import EventSpotlight from '../../components/EventSpotlight'
 import Example from '../../components/Example'
 import Layout from '../../components/Layout'
 
@@ -38,13 +39,38 @@ const Events = () => {
       description:
         ':S DjJ ;lakJD;flkds;lfja;mx clkvnsghsdfja;dlskjfa;dlkj fa;lkjd ;alknvlcnva;lsdn fasdf;laskjd f;alkmvlxcnv;',
     },
+    {
+      date: moment('31 Oct 2021'),
+      location: 'Yusuf Ishak House;ladls',
+      title: 'It will be here',
+      description:
+        ':S DjJ ;lakJD;flkds;lfja;mx clkvnsghsdfja;dlskjfa;dlkj fa;lkjd ;alknvlcnva;lsdn fasdf;laskjd f;alkmvlxcnv;'.repeat(
+          10
+        ),
+    },
+    {
+      date: moment(),
+      location: 'NUS School of Computing',
+      title: 'It will be there',
+      description:
+        ':S DjJ ;lakJD;flkds;lfja;mx clkvnsghsdfja;dlskjfa;dlkj fa;lkjd ;alknvlcnva;lsdn fasdf;laskjd f;alkmvlxcnv;',
+    },
   ]
   return (
     <Layout>
       <Example />
       <Container maxWidth="lg">
+        <Box>
+          <Box style={{ width: '40em', height: '20em' }}>Calendar</Box>
+          <EventSpotlight
+            date={events.at(0).date}
+            location={events.at(0).location}
+            title={events.at(0).title}
+            description={events.at(0).description}
+          />
+        </Box>
         <Box className={classes.eventsList}>
-          {events.map((ev, index) => (
+          {events.splice(1).map((ev, index) => (
             <EventCard
               key={index}
               date={ev.date}
