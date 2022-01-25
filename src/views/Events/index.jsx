d1adf083daa8af69212ea5c195971a6e0ee892b0
Ivander <ivanderjmw@gmail.com>
2022-01-25 21:26:55 +0800

    justify events to center

--- src/views/Events/index.jsx
+++ src/views/Events/index.jsx
@@ -15,6 +15,7 @@ const useStyles = makeStyles((theme) => ({
     gap: '2em',
     flexWrap: 'wrap',
     alignItems: 'stretch',
+    justifyContent: 'center',
     flexDirection: 'row',
     height: 'max-content',
     [theme.breakpoints.down('xs')]: {
@@ -98,6 +99,37 @@ const events = [
   {
     id: 4,
     title: 'Tomorrow',
+    start: new Date(new Date().setDate(new Date().getDate() + 1)),
+    end: new Date(new Date().setDate(new Date().getDate() + 1)),
+    description: 'Near to tomorrow',
+    location: 'nus',
+  },
+  {
+    id: 12,
+    title: 'XIt will be there yeah',
+    location: 'NUS School of Computing',
+    date: new Date('01/27/2022'),
+    description:
+      ':S DjJ ;lakJD;flkds;lfja;mx clkvnsghsdfja;dlskjfa;dlkj fa;lkjd ;alknvlcnva;lsdn fasdf;laskjd f;alkmvlxcnv;',
+    allDay: true,
+    start: new Date('01/27/2022 01:00'),
+    end: new Date('01/27/2022 02:00'),
+    imageUrl:
+      'https://images.unsplash.com/photo-1643037906067-e79f62e8f58c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
+  },
+  {
+    id: 13,
+    title: 'XToday',
+    start: new Date(new Date().setHours(new Date().getHours() - 3)),
+    end: new Date(new Date().setHours(new Date().getHours() + 3)),
+    description: 'Near to today',
+    location: 'here',
+    imageUrl:
+      'https://images.unsplash.com/photo-1524758631624-e2822e304c36?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
+  },
+  {
+    id: 14,
+    title: 'XTomorrow',
     start: new Date(new Date().setDate(new Date().getDate() + 1)),
     end: new Date(new Date().setDate(new Date().getDate() + 1)),
     description: 'Near to tomorrow',
